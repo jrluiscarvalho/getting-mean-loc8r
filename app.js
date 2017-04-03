f268c9752c80464b6683ff6578c39926873fd929
luis <jrluiscarvalho@gmail.com>
2017-04-03 00:46:22 -0300

    continous api

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/api', routesApi);
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
---
+++ app_api/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlReviews = require('../controllers/reviews');
+
+//estabelecimentos (locations)
+router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/locations', ctrlLocations.locationsCreate);
+router.get('/locations/:locationId', ctrlLocations.locationsReadOne);
+router.put('/locations/:locationId', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationId', ctrlLocations.locationsDeleteOne);
+
+//resenhas (reviews)
+router.post('/locations/:locationId/reviews', ctrlReviews.reviewsCreate);
+router.get('/locations/:locationId/reviews/:reviewId', ctrlReviews.reviewsReadOne);
+router.put('/locations/:locationId', ctrlReviews.reviewsUpdateOne);
+router.delete('/locations/:locationId', ctrlReviews.reviewsDeleteOne);
