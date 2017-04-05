f9f3c887f72cf07e41171effb60ac698b9859bb0
luis <jrluiscarvalho@gmail.com>
2017-04-05 02:03:57 -0300

    api in progress

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
 var users = require('./app_server/routes/users');
 
 var app = express();
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,14 +4,16 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlReviews = require('../controllers/reviews');
 
 //estabelecimentos (locations)
-router.get('/locations', ctrlLocations.locationsListByDistance);
-router.post('/locations', ctrlLocations.locationsCreate);
-router.get('/locations/:locationId', ctrlLocations.locationsReadOne);
-router.put('/locations/:locationId', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationId', ctrlLocations.locationsDeleteOne);
+//router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/location', ctrlLocations.locationsCreate);
+//router.get('/locations/:locationId', ctrlLocations.locationsReadOne);
+//router.put('/locations/:locationId', ctrlLocations.locationsUpdateOne);
+//router.delete('/locations/:locationId', ctrlLocations.locationsDeleteOne);
 
-//resenhas (reviews)
-router.post('/locations/:locationId/reviews', ctrlReviews.reviewsCreate);
-router.get('/locations/:locationId/reviews/:reviewId', ctrlReviews.reviewsReadOne);
-router.put('/locations/:locationId', ctrlReviews.reviewsUpdateOne);
-router.delete('/locations/:locationId', ctrlReviews.reviewsDeleteOne);
+ //resenhas (reviews)
+//router.post('/locations/:locationId/reviews', ctrlReviews.reviewsCreate);
+//router.get('/locations/:locationId/reviews/:reviewId', ctrlReviews.reviewsReadOne);
+//router.put('/locations/:locationId', ctrlReviews.reviewsUpdateOne);
+//router.delete('/locations/:locationId', ctrlReviews.reviewsDeleteOne);
+
+ module.exports = router;
