faecfab308d1bacf92d32455205f49aab987dc17
luis <jrluiscarvalho@gmail.com>
2017-04-09 17:48:16 -0300

    api modifies

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,5 +1,34 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
+var request = require('request');
+
+var apiOptions = {
+    server: "http://localhodt:3000"
+};
+if(process.env.NODE_ENV === 'production'){
+    apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
+}
+
+var requestOptions = {
+    url: "http://yourapi.com/api/path",
+    method: "GET",
+    json:{},
+    qs:{
+        offset:20
+    }
+};
+
+request(requestOptions, function(err, response, body){
+    if(err){
+        console.log(err);
+    }else if(response.statusCode === 200){
+        console.log(body);
+    }else{
+        console.log(response.statusCode);
+    }
+});
+
+
 
 var sendJsonResponse = function(res, status, content){
     res.status(status);
@@ -355,3 +384,36 @@ module.exports.reviewsDeleteOne = function(req, res){
             }
         );
 };
+
+let renderHomepage = function(req, res, reponseBody){
+    res.render('locations-list', {
+        'title':'Loc8r - find a place to work with wifi',
+        pageHeader:{
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "looking for wifi and a seat? loc8r helps you find places to work when out and about",
+        locations: responseBody
+    });
+};
+
+module.exports.homelist = function(req, res){
+    let requestOptions, path;
+    path = '/api/locations';
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {},
+        qs:{
+            lng: -0.7992599,
+            lat: 51.378091,
+            maxDistance: 20
+        }
+    };
+    request(
+        requestOptions,
+        function(err, response, body){
+            renderHomepage(req, res);
+        }
+    );
+}
